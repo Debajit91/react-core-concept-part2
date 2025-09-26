083387ba4e570453f7637457f0e442e63ecf6672
Debajit91 <debajitroy544@gmail.com>
2025-09-26 20:54:28 +0600

    added

--- src/Users.jsx
+++ src/Users.jsx
@@ -4,6 +4,7 @@ export default function Users({fetchUsers}){
 
     const users = use(fetchUsers);
     console.log(users);
+    
     return(
         <div className="card">
             <h3>Users: {users.length}</h3>
